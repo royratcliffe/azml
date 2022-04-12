d5f1938bb01b633fe459582e83ae20c7e86fb1eb
Roy Ratcliffe <royratcliffe@me.com>
2022-04-12 06:56:56 +0100

    refactor: workspace and resource methods

--- R/az-resource-group.R
+++ R/az-resource-group.R
@@ -1,13 +1,3 @@
-#' Get Workspace
-#'
-#' @param x Something to get a workspace from, typically a resource group.
-#' @param ... Extra parameters to pass.
-#'
-#' @export
-get_workspace <- function(x, ...) {
-  UseMethod("get_workspace")
-}
-
 #' Get Workspace from Resource Group
 #'
 #' Uses a resource group's subscription identifier and resource group name to
@@ -25,29 +15,9 @@ get_workspace.az_resource_group <- function(x, name, ...) {
   )
 }
 
-#' List Machine-Learning Workspaces
-#'
-#' @param x Something to get a workspace from, typically a resource group.
-#' @param ... Extra parameters to pass.
-#'
-#' @export
-list_workspaces <- function(x, ...) {
-  UseMethod("list_workspaces")
-}
-
 #' @export
 list_workspaces.az_resource_group <- function(x, ...) {
   list_resources_by_type(x, "Microsoft.MachineLearningServices/workspaces")
-}
-
-#' List Resources by Type
-#' @param x Resource group.
-#' @param resource_type Type of resource to list.
-#' @param ... Extra parameters passed to \code{list_resources} method of Azure
-#'   resource group.
-#' @export
-list_resources_by_type <- function(x, resource_type, ...) {
-  UseMethod("list_resources_by_type")
 }
 
 #' @export
